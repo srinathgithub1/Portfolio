f5367c38b19b579afe4575475a329b73e764c492
Srinath <srinathkmr0031@gmail.com>
2024-05-31 03:24:18 +0530

    Added side bare when its will open in moblie phone

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const h2Ele=document.querySelector(".animation");
-const careers=["Web Developer", "Competitive Coder", "Instructor", "Freelancer"];
+const careers=["Web - Developer", "Competitive Coder", "Instructor", "Freelancer"];
 let ind=0;
 let charind=0;
 updateText();
@@ -13,5 +13,18 @@ function updateText(){
     if(ind===careers.length){
         ind=0;
     }
-    setTimeout(updateText,400);
+    setTimeout(updateText,200);
 }
+
+// menu bar
+const root=document.querySelector(":root");
+const bar=document.querySelector(".open-bar");
+const cross=document.querySelector("#cross");
+bar.addEventListener("click",()=>{
+    root.style.setProperty("--bar-right","0px");
+    bar.style.display="none";
+});
+cross.addEventListener("click",()=>{
+    root.style.setProperty("--bar-right","-150px");
+    bar.style.display="block";
+})
